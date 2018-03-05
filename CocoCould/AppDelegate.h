eff5f2d7ca91b8c808a9490f92d282861078bafa
Lu Wei Jian <374265237@qq.com>
2018-03-05 23:27:36 +0800

    Initial Commit

---
+++ CocoCould/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoCould
+//
+//  Created by JayJay on 2018/3/5.
+//  Copyright © 2018年 JayJay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocoCould/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoCould
+//
+//  Created by JayJay on 2018/3/5.
+//  Copyright © 2018年 JayJay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
